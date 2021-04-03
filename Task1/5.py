6283d9725de441810f68c8f6ceb0cf0d48b17d4b
Willenst <67371653+Willenst@users.noreply.github.com>
2021-04-03 23:23:12 +0200

    4

--- Task1/5.py
+++ Task1/5.py
@@ -1,31 +1,26 @@
 '''5. В текстовом файле input.txt записан русский текст.
 Найти в тексте слова, содержащие подряд какие-нибудь две из четырех наиболее часто встречающихся букв текста,
 записать эти слова заглавными буквами и указать после каждого такого слова в скобках найденные буквы.'''
-
-letters='абвгдежзийклмнопрстуфхцчшщъыьэюя'
+import re
 
 f = open('input.txt', 'r')
 l = str([line.strip() for line in f])
 a = str(l.casefold())
+a=re.sub('[,.]','',a)
 words = a.split(" ")
+a=a.replace(' ','')
 
-b=list()
-for letter in letters:
-    b.append(a.count(letter))
-b.sort(reverse = True)
-b=b[0:4]
+A = {}
 
-for letter in letters:
-    if a.count(letter) in b:
-        b.append(letter)
-Most_often=b[4:8]
-print('Наиболее часто встречаются:',Most_often)
-print(words)
+for letter in a:
+    A[letter]=A.get(letter,0)+1
 
-print(Most_often)
-Most = ''.join(Most_often)
+
+P=sorted(A.items(),key=lambda x: x[1])[::-1][0:4]
+G=('[{}{}{}{}]'.format(P[0][0],P[1][0],P[2][0],P[3][0]))+'{2}'
+
+print(G)
 for word in words:
-    for i in range(3):
-        if Most[i]+Most[i+1] in word:
-            letters2='('+Most[i]+Most[i+1]+')'
-            print(word.upper(),letters2)
+    if len(re.findall(G,word)):
+        print(word.upper()+'('+re.findall(G,word)[0]+')')
+
